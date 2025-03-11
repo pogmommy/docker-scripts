48d4b0f52dad7da6d0cc9feeeae5dfd639c93f9e
Penelope Gwen <support@pogmom.me>
2025-03-11 11:09:52 -0700

    dont erase .env values

--- template_prep.sh
+++ template_prep.sh
@@ -3,4 +3,4 @@
 find "${HOME}/Dockers/" -iname '*.env' -not -iname '.*' -exec cp "{}" "{}_template" \;
 cp "${HOME}/Dockers/.env" "${HOME}/Dockers/.env_template"
 find "${HOME}/Dockers/" -iname '*.env_template' -exec sed -i 's/=.*/=/' {} \;
-sed -i 's/=.*/=/' "${HOME}/Dockers/.env"
+sed -i 's/=.*/=/' "${HOME}/Dockers/.env_template"
